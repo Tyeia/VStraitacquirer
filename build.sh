166c885942f488fc52810162bf295941a4df8bee
Tyeia <tyeia6633@gmail.com>
2026-05-19 00:38:32 -0400

    Fix #6

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
 dotnet run --project ./CakeBuild/CakeBuild.csproj -- "$@"
-cd ./traitacquirer/bin/Release/Mods/mod/publish/
-zip ../../../../../../traitacquirermoddedclasses-0.9.8.zip -r * 
-cd ../../../../../../
